524a700f8c19b94fb7bc3c61dfd6e0d23507a788
himkwan01 <himkwanbbq@hotmail.com>
2015-09-17 17:57:55 -0700

    AS 2 and AS 3

--- Hmwk/AS2/main.cpp
+++ Hmwk/AS2/main.cpp
@@ -15,11 +15,27 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-  SimpleVector<int> test();
-//  cout<<test.size()<<endl;
-//  test.push_back(4);
-//  cout<<test.size()<<" "<<test[0]<<endl;
+  cout<<"Vector size 10 created\n";
   
+  SimpleVector<int> test(10);
+  
+  cout<<"Vector size = "<<test.size()<<endl;
+  for(int i=0;i<test.size();i++){
+    test[i]=i;
+  }
+  cout<<"push back value 4\n";
+  test.push_back(4);
+  
+  cout<<"Vector size = "<<test.size()<<endl;
+  for(int i=0;i<test.size();i++){
+    test[i]=i;
+  }
+  cout<<"pop back the last element\n";
+  test.pop_back();
+  cout<<"Vector size = "<<test.size()<<endl;
+  for(int i=0;i<test.size();i++){
+    test[i]=i;
+  }
   return 0;
 }
 
